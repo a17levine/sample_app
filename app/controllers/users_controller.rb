f2a191b25b4089e6430ff93696726a8cbe0b47b0
Alex Levine <a17levine@gmail.com>
2013-04-13 15:37:10 -0500

    end of chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,5 +16,4 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
-
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -19,12 +19,21 @@ describe "UserPages" do
   end
 
   describe "signup" do
-    before { visit signup_path }
+    before { visit signup_path } #should this line be here?
     let(:submit) {"Create my account"}
+
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before {click_button submit}
+
+        it { should have_selector('title', text: 'Sign up')}
+        it { should have_content('error')}
+      end
+
     end
 
     describe "with valid information" do
@@ -34,6 +43,15 @@ describe "UserPages" do
         fill_in "Password",         with: "foobar"
         fill_in "Confirmation",     with: "foobar"
       end
+
+      describe "after saving the user" do
+        before {click_button submit}
+        let(:user) {User.find_by_email('user@example.com')}
+
+        it {should have_selector('title', text: user.name)}
+        it {should have_selector('div.alert.alert-success', text: 'Welcome')}
+      end
+
 
       it "should create a user" do
         expect {click_button submit}.to change(User, :count).by(1)
